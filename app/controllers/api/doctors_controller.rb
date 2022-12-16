6a1e1733383e7dad2174abae65448a74841e8459
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-15 19:37:29 -0700

    doc has a control

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,16 +1,44 @@
 class Api::DoctorsController < ApplicationController
+ before_action :set_doctors, only [:show, :update, :destroy]
+ 
   def index
+    render json: Doctor.all
   end
 
   def show
+    @doctor = Doctor.find(params[:id])
+    render json: @doctor
   end
 
   def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      render json: @doctor
+    else
+      render json: {error: @doctor.error },  status: :unprocessable_entity
+      end
   end
 
   def update
+    if @doctor.update(doctor_params)
+      render json: @doctor
+    else
+      render json: {error: @doctor.error },  status: :unprocessable_entity
   end
 
   def destroy
+    @doctor.destroy
+    render json:{ message: 'Bye Bye Doc'}
+    end
+
+  def private
+    def doctor_params
+      params.require(:doctor).permit(:first_name, :last_name, :practice)
+    end
+    
+    def set_doctors
+      @doctor = Doctor.find(params[:id])
+    end
+
   end
 end
