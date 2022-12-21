3fddb01b1a5547e4e1c6d863cd1fdb444a5a6bc4
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-21 16:11:20 -0700

    app.js now has users and doctors added but there are bugs in our code both in users and in doc stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/shared/Home';
 import MainNavBar from './components/shared/MainNavbar';
 import NoMatch from './components/shared/NoMatch';
+import Doctors from './components/doctors/Doctors';
+import Users from './components/users/Users'
 
 const App = () => (
   <>
     <MainNavBar />
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/*' element={<NoMatch />} />
+      <Route path='/' element={<Home />}/>
+      <Route path='/*' element={<NoMatch />}/>
+      <Route path='/doctors' element={<Doctors/>}/>
+      <Route path='/users' element={<Users/>}/>
     </Routes>
   </>
 )
