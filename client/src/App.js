8313a65536cb92c9be067cdb1fdde80cbbecbff9
Daylan Merritt <daylan.merritt@gmail.com>
2022-12-21 17:28:04 -0700

    resolved

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,24 @@ import NoMatch from './components/shared/NoMatch';
 import Doctors from './components/doctors/Doctors';
 import Users from './components/users/Users'
 import Users from './components/users/Users';
-import UserShow from './components/users/UserShow';
+import DoctorShow from './components/doctors/doctorShow'
 
 const App = () => (
   <>
     <MainNavBar />
     <Routes>
+<<<<<<< HEAD
       <Route path='/' element={<Home />}/>
       <Route path='/*' element={<NoMatch />}/>
       <Route path='/doctors' element={<Doctors/>}/>
       <Route path='/users' element={<Users/>}/>
       <Route path='/users/:id' element={<UserShow />} />
+=======
+      <Route path='/' element={<Home />} />
+      <Route path='/*' element={<NoMatch />} />
+      <Route path='/users' element={<Users />} />
+      <Route path='/:userId/doctor' element={<DoctorShow />} />
+>>>>>>> fd1f0b0 (stuff)
     </Routes>
   </>
 )
